9dc57ebf62f4b2fcd8272b5b2799486141cceb38
Easysale <32016187+Easysale@users.noreply.github.com>
2025-11-03 20:44:25 +0300

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center text-center">
-      <h1 className="text-4xl font-bold mb-4 text-blue-700">Welcome to Topville Hotel Mombasa</h1>
+      <h1 className="text-4xl font-bold mb-4 text-blue-700">
+        Welcome to Topville Hotel Mombasa
+      </h1>
       <p className="text-gray-600 mb-6 italic">Relax. Refresh. Reconnect.</p>
       <a
         href="/rooms"
