221b077f722f278023220d81d601b5c490a9a0e2
Easysale <32016187+Easysale@users.noreply.github.com>
2025-11-03 16:36:45 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Home.jsx'
+import Rooms from './pages/Rooms.jsx'
 import './styles.css'
 import Home from './pages/Home'
 createRoot(document.getElementById('root')).render(
