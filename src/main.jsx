347b6a08d30c8374456acf1fab7f370221c75b3c
Easysale <32016187+Easysale@users.noreply.github.com>
2025-11-03 21:10:55 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,2 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home.jsx";
-import Rooms from "./pages/Rooms.jsx";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/rooms" element={<Rooms />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+import "./index.css"
+console.log("✅ main.jsx loaded")
