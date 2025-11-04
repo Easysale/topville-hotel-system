cba30309e2284b8deb02e431bfbee6d1c193d801
Easysale <32016187+Easysale@users.noreply.github.com>
2025-11-04 11:20:28 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,10 @@ import Footer from "./components/Footer.jsx";
 import Home from "./pages/Home.jsx";
 import Rooms from "./pages/Rooms.jsx";
 import "./index.css";
+import Contact from "./pages/Contact";
+
+<Route path="/contact" element={<Contact />} />
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
