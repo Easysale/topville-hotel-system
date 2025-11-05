43e89087f946f33eef31fdae35858d61ee0c2440
Easysale <32016187+Easysale@users.noreply.github.com>
2025-11-05 08:46:52 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
-import Footer from "./components/Footer.jsx";
 import Home from "./pages/Home.jsx";
-import Rooms from "./pages/Rooms.jsx";
-import Gallery from "./pages/Gallery.jsx";
-import Contact from "./pages/Contact.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/rooms" element={<Rooms />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
+      <Home />
     </BrowserRouter>
   </React.StrictMode>
 );
